68cc51f7f524b4bcc5ad3b3de14a3b00e663f4e7
tintupratap <39707816+tintupratap@users.noreply.github.com>
2020-05-24 13:54:23 +0530

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,4 +1,4 @@
 pkg up
-pkg install build-essential python curl neofetch termux-api htop
+pkg install build-essential python curl neofetch termux-api htop openssh
 pip install --upgrade pip
 pip install CherryPy wheel
